38c245747248e06fc3410335554f7195cd5d0cb1
Aco <Aco@DESKTOP-O0QL2VH>
2019-02-02 13:43:35 +0100

    test2

---
+++ projekt_grupp_5/src/projekt_grupp_5/GUI.java
@@ -0,0 +1,66 @@
+package projekt_grupp_5;
+
+import java.awt.*;
+import java.awt.event.*;
+
+
+import javax.swing.*;
+
+
+public class GUI {
+	private JFrame frame;
+    private Container contentPane;
+	public GUI() {
+		makefram();
+	}
+
+	private void makefram() {
+		frame = new JFrame("FLAPPY BIRDS");
+		contentPane = frame.getContentPane();
+		//makeMenuBar(frame);
+		//skapa paneler
+		makePanels(contentPane);
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	public void makePanels(Container contentPane) {
+		contentPane.setLayout(new GridLayout(3,2,3,3));
+		JButton start = new JButton("START");
+
+
+		//procedur när start trycks
+		start.addActionListener(
+				(ActionEvent e) -> {test(); }
+				);
+		contentPane.add(start);
+
+
+		JButton HS = new JButton("HIGH SCORE");
+		HS.addActionListener(
+				(ActionEvent e) -> {test2(); }
+				);
+
+
+		contentPane.add(HS);
+
+
+	}
+
+	private void test() {
+		System.out.println("testar START \n");
+		//hur tar man bort gridlayouten och startar spelet
+		
+		contentPane.removeAll();
+		contentPane.repaint();
+		
+		//anropa funktion som startar spelet
+		
+		contentPane.add(new JLabel("test"));
+		
+		
+	}
+
+	private void test2() {
+		System.out.println("testar HIGH SCORE \n");}
+}
