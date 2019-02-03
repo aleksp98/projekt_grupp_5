50f0ba3c9dde4ef4900600775e28276b3fdcff10
Mozar <Mozar@DESKTOP-44333LK>
2019-02-03 23:59:42 +0100

    test2

---
+++ projekt_grupp_5/src/projekt_grupp_5/Bird.java
@@ -0,0 +1,15 @@
+package projekt_grupp_5;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Bird extends JLabel {
+	private ImageIcon test;
+	private int  y_position;
+	private int  x_position;
+	
+	public Bird(ImageIcon test) 
+	{
+		super(test);
+	}
+}
