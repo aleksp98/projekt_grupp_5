5bf5656885cd1ad9b9d460b816b526a45d89f895
Aco <Aco@DESKTOP-O0QL2VH>
2019-02-04 19:02:05 +0100

    Bird moving

--- projekt_grupp_5/src/projekt_grupp_5/GUI.java
+++ projekt_grupp_5/src/projekt_grupp_5/GUI.java
@@ -5,9 +5,14 @@ import java.awt.event.*;
 import javax.swing.*;
 
 
-public class GUI {
+public class GUI implements KeyListener, ActionListener  {
 	private JFrame frame;
 	private Container contentPane;
+	private Bird untz;
+    private Dimension size;
+    private int y_pos = 200;
+    Timer timer = new Timer(300,this); // Swing-timer
+	
 	public GUI() {
 		makefram();
 	}
@@ -16,9 +21,14 @@ public class GUI {
 		frame = new JFrame("FLAPPY BIRDS");
 		contentPane = frame.getContentPane();
 		//makeMenuBar(frame);
+		
+		 
 		//skapa paneler
 		makePanels(contentPane);
+		 
+		frame.addKeyListener(this);
 		frame.pack();
+		frame.setSize(1000,1000);
 		frame.setVisible(true);
 	}
 
@@ -43,18 +53,48 @@ public class GUI {
 		System.out.println("testar START \n");
 		
 		contentPane.removeAll();
-		contentPane.repaint();
+		
 		//anropa funktion som startar spelet
 		//game.start();
 		
 		//skapa bird
 		ImageIcon img = new ImageIcon(this.getClass().getResource("/bird.png"));
-		Bird untz = new Bird(img);
+		
+		untz = new Bird(img);
+		
+		 size = untz.getPreferredSize();
+	  
+		 untz.setBounds(400, 200, size.width, size.height);
+		 
 		contentPane.add(untz);
+		
+		timer.start();
+		
+		contentPane.repaint();
 	}
 
 	//funktion för att trigga HighScore
 	private void HS() {
 		System.out.println("testar HIGH SCORE \n");}
+	
+	 public void keyReleased(KeyEvent e) {}
+	    public void keyTyped(KeyEvent e) {}
+	    
+	    public void actionPerformed(ActionEvent e) {
+	    	y_pos = y_pos + 10;
+        	untz.setBounds(400, y_pos, size.width, size.height);
+        	contentPane.repaint(); 
+	    	 } 
+	
+	
+	 @Override
+	    public void keyPressed(KeyEvent e) {
+	        if (e.getKeyCode() == KeyEvent.VK_UP) {
+	    		System.out.println("testar key UP \n");
+	        	y_pos = y_pos - 10;
+	        	untz.setBounds(400, y_pos, size.width, size.height);
+	        	contentPane.repaint(); 
+	        }
+	    }
 	
 }
